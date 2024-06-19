0c1e0572f48b8cf47ef90f462a75fac110e35e8b
Wayne Oxford-Huggett <wayne@oxfordhuggett.co.za>
2024-06-19 06:11:08 +0200

    Update adding R support.

--- init.lua
+++ init.lua
@@ -835,7 +835,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'markdown_inline', 'r', 'rnoweb' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -862,6 +862,41 @@ require('lazy').setup({
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
+  },
+  {
+    'R-nvim/R.nvim',
+    config = function()
+      -- Create a table with the options to be passed to setup()
+      local opts = {
+        R_args = { '--quiet', '--no-save' },
+        hook = {
+          on_filetype = function()
+            -- This function will be called at the FileType event
+            -- of files supported by R.nvim. This is an
+            -- opportunity to create mappings local to buffers.
+            vim.api.nvim_buf_set_keymap(0, 'n', '<Enter>', '<Plug>RDSendLine', {})
+            vim.api.nvim_buf_set_keymap(0, 'v', '<Enter>', '<Plug>RSendSelection', {})
+          end,
+        },
+        min_editor_width = 72,
+        rconsole_width = 78,
+        disable_cmds = {
+          'RClearConsole',
+          'RCustomStart',
+          'RSPlot',
+          'RSaveClose',
+        },
+      }
+      -- Check if the environment variable "R_AUTO_START" exists.
+      -- If using fish shell, you could put in your config.fish:
+      -- alias r "R_AUTO_START=true nvim"
+      if vim.env.R_AUTO_START == 'true' then
+        opts.auto_start = 1
+        opts.objbr_auto_start = true
+      end
+      require('r').setup(opts)
+    end,
+    lazy = false,
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
